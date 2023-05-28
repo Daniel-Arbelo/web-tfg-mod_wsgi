60ccfef1ca6195837cb9f34a0fb75de65141bb68
Daniel Arbelo <alu0101117621@ull.edu.es>
2023-05-28 14:25:21 +0100

    Umpdate for mod-wsdi

---
+++ test_script.py
@@ -0,0 +1,11 @@
+def application(environ,start_response):
+    status = '200 OK'
+    html = '\n' \
+           '\n' \
+           ' mod_wsgi is perfectly working\n' \
+           '\n' \
+           '\n'
+    response_header = [('Content-type','text/html')]
+    start_response(status,response_header)
+    return [html]
+
