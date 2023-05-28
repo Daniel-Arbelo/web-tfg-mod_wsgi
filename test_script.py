d4563d7bb05cc34fc1c45041f30929f89a5c42e5
Daniel Arbelo <alu0101117621@ull.edu.es>
2023-05-28 14:12:19 +0100

    ejecutando rec de imag

--- test_script.py
+++ test_script.py
@@ -1,8 +1,20 @@
-def application(environ,start_response):
+import os
+
+
+def application(environ,start_response): 
+    # Execute the comman
+    programa = 'echo \'Z7ZhekVI\' | sudo -S -u daniel python /var/www/html/detectnet-console.py /home/daniel/jetson-inference/build/aarch64/bin/images/peds_0.jpg /var/www/html/imagenesPasadasPrograma/output0.jpg'
+    #programa = 'sudo ls /root'
+    
+    # Ejecutar el programa externo y capturar la salida
+     
+    output = os.popen(programa).read()
+    
     status = '200 OK'
     html = '\n' \
            '\n' \
-           ' mod_wsgi is perfectly working\n' \
+           ' mod_wsgi is working\n' \
+           +output+ \
            '\n' \
            '\n'
     response_header = [('Content-type','text/html')]
