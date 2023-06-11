a350ddc872198573e8d6411043393fea43d8a39c
Daniel Arbelo <alu0101117621@ull.edu.es>
2023-06-11 19:22:28 +0100

    programa coordenadas

--- pythonHTTPservers/info-controladora.py
+++ pythonHTTPservers/info-controladora.py
@@ -1,62 +1,19 @@
 import os
-import time
-import signal
-import multiprocessing
-
-def ejecutar_programa():
-        # Abrir el archivo para escribir la salida
-        archivo_salida = open("/var/www/html/salida_programa_coordenadas.txt", "w")
-        
-        # Redirigir la salida estandar al archivo
-        os.dup2(archivo_salida.fileno(), 1)
-        
-        # Llamar al programa
-        programa = 'echo \'Z7ZhekVI\' | sudo -S -u daniel python3 /var/www/html/coordenadas.py'
-        os.system(programa)
-
-        # Cerrar el archivo de salida
-        archivo_salida.close()
 
 def application(environ,start_response): 
+    programa = 'echo \'Z7ZhekVI\' | sudo -S -u daniel python3 /var/www/html/coordenadas.py'
     
-    # Crear el proceso
-    proceso = multiprocessing.Process(target=ejecutar_programa)
+    # Ejecutar el programa externo y capturar la salida
+    output = os.popen(programa).read()
 
-    # Iniciar el proceso
-    proceso.start()
-
-    # Esperar 10 segundos o hasta que el proceso termine
-    proceso.join(timeout=10)
-
-    # Verificar si el proceso esta todavia en ejecucion
-    if proceso.is_alive():
-        # El proceso aun no ha terminado, asi que lo terminamos
-        proceso.terminate()
-        proceso.join()  # Esperar a que el proceso termine completamente
-        
-        status = '200 OK'
-        html = '\n' \
+    status = '200 OK'
+    html = '\n' \
             '\n' \
-            'El programa no respondio en 10 segundos. Se ha terminado.\n' \
+            +output+ \
             '\n' \
             '\n'
-        response_header = [('Content-type','text/html')]
-        start_response(status,response_header)
-        return [html]
-    else:
-        # El proceso ha terminado dentro del tiempo limite
-
-        # Leer la salida del programa desde el archivo
-        with open("/var/www/html/salida_programa_coordenadas.txt", "r") as archivo_salida:
-            salida = archivo_salida.read()
-
-        status = '200 OK'
-        html = '\n' \
-            '\n' \
-            + salida + \
-            '\n' \
-            '\n'
-        response_header = [('Content-type','text/html')]
-        start_response(status,response_header)
-        return [html]
+    response_header = [('Content-type','text/html')]
+    start_response(status,response_header)
+    return [html]
+    
         
